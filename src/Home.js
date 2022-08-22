b5ee21c9c3e9f17b4f6c71cb3ed08fba4aade4e8
EJ-David <daviderhabor.de@gmail.com>
2022-08-22 20:23:49 +0100

    Api all set up, HTML manipulation next

--- src/Home.js
+++ src/Home.js
@@ -1,68 +1,98 @@
-import './App.css';
-import { BiSearchAlt }  from 'react-icons/bi'
+import "./App.css";
+//import { BiSearchAlt } from "react-icons/bi";
+import useFetch from "./useFetch";
+import { useState, useMemo } from "react";
+
+//https://covidnigeria.herokuapp.com/api
 
 function Home() {
+  const [stateData, setStateData] = useState(null);
+  const [inputState, setInputState] = useState("");
+
+  const { data, isPending } = useFetch(
+    "https://covidnigeria.herokuapp.com/api"
+  );
+
+  const stats = useMemo(() => data, [data]);
+
+  if (!isPending && !stateData) {
+    setStateData(stats.data.states);
+  }
+
+  console.log(stateData);
+  console.log(inputState);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //check if submitted word matches any state name, display match in html
+  };
+
   return (
-    <div className='home'>
-     <h1>Nigeria's COVID-19 Statistics</h1>
-     <p className='info'>This app is designed to search and display the statistics of Covid-19 cases in Nigeria, using an open API.</p>
-        
-        <div className='search'>
-          <input type="text" name="search" placeholder="Enter search keyword"></input>
-          <button type="submit"><BiSearchAlt/></button>
+    <div className="home">
+      <h1>Nigeria's COVID-19 Statistics</h1>
+      <p className="info">
+        This app is designed to search and display the statistics of Covid-19
+        cases in Nigeria, using an open API.
+      </p>
+
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          value={inputState}
+          onChange={(e) => setInputState(e.target.value)}
+          placeholder="Enter state"
+        ></input>
+        <button type="submit">Submit Form</button>
+      </form>
+
+      <h2>Total Statistics of Covid-19 Cases and Death in Nigeria</h2>
+
+      <div className="total">
+        <div>
+          <h4>Samples Tested</h4>
+          <p>43948398</p>
         </div>
-        
-
-        <h2>Total Statistics of Covid-19 Cases and Death in Nigeria</h2>
-
-        <div className='total'>
-          <div>
-            <h4>Samples Tested</h4>
-            <p>43948398</p>
-          </div>
-          <div>
-            <h4>Confirmed Cases</h4>
-            <p>1809212</p>
-          </div>
-          <div>
-            <h4>Active Cases</h4>
-            <p>4348389</p>
-          </div>
-          <div>
-            <h4>Discharged</h4>
-            <p>390293</p>
-          </div>
-          <div>
-            <h4>Death</h4>
-            <p>48430309</p>
-          </div>
-          
+        <div>
+          <h4>Confirmed Cases</h4>
+          <p>1809212</p>
         </div>
+        <div>
+          <h4>Active Cases</h4>
+          <p>4348389</p>
+        </div>
+        <div>
+          <h4>Discharged</h4>
+          <p>390293</p>
+        </div>
+        <div>
+          <h4>Death</h4>
+          <p>48430309</p>
+        </div>
+      </div>
 
-        <h3>Lagos</h3>
+      <h3>Lagos</h3>
 
-        <div className='results'>
-          <div>
-            <h4>Confirmed Cases</h4>
-            <p>3298030</p>
-          </div>
-          <div>
-            <h4>Cases on Admission</h4>
-            <p>28328332</p>
-          </div>
-          <div>
-            <h4>Discharged</h4>
-            <p>3939209</p>
-          </div>
-          <div>
-            <h4>Death</h4>
-            <p>9493039</p>
-          </div>
-          
-        </div>  
-
+      <div className="results">
+        <div>
+          <h4>Confirmed Cases</h4>
+          <p>3298030</p>
+        </div>
+        <div>
+          <h4>Cases on Admission</h4>
+          <p>28328332</p>
+        </div>
+        <div>
+          <h4>Discharged</h4>
+          <p>3939209</p>
+        </div>
+        <div>
+          <h4>Death</h4>
+          <p>9493039</p>
+        </div>
+      </div>
     </div>
-  
   );
 }
 
---
+++ src/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
+    const [errMessage, setErrMessage] = useState(null);
+
+
+ 
+
+   useEffect(() => {
+
+    const abortCont = new AbortController();
+        fetch(url, {signal: abortCont.signal})
+            .then(res => {
+                //console.log(res)
+                if(!res.ok ){
+                    throw Error('data resource not okay') 
+                }return res.json() ;
+                
+            })
+            .then((data) => {
+                 setBlogs(data)
+                 setIsPending(false)
+                 setErrMessage(null)
+            })
+            .catch((err) => {
+                if(err.name === 'AbortError'){
+                    console.log('fetch aborted')
+                }else{
+                    setErrMessage(err.message)
+                    setIsPending(false)
+                }
+                 
+                
+            })
+
+    return () => {abortCont.abort()}
+   }, [url])
+    
+    return { data, isPending, errMessage};
+}
+ 
+export default useFetch;
