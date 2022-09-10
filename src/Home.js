d1c2ba9e928417591cae2e3cbb667819b846b121
EJ-David <daviderhabor.de@gmail.com>
2022-09-10 23:46:28 +0100

    attempted to handle errors to prevent white screen

--- src/Home.js
+++ src/Home.js
@@ -11,57 +11,55 @@ class DisplayStats extends Component {
     return (
       <div className="country-stats wrapper">
         <h2>Nigeria</h2>
-        
-          <div className="data-box">
-            <p>{data.totalSamplesTested}</p>
-            <h4>Samples Tested</h4>
-          </div>
-          <div className="data-box">
-            <p>{data.totalConfirmedCases}</p>
-            <h4>Confirmed Cases</h4>
-          </div>
-          <div className="data-box">
-            <p>{data.totalActiveCases}</p>
-            <h4>Active Cases</h4>
-          </div>
-          <div className="data-box">
-            <p>{data.discharged}</p>
-            <h4>Discharged</h4>
-          </div>
-          <div className="data-box">
-            <p>{data.death}</p>
-            <h4>Deaths</h4>
-          </div>
-      </div>
-    );
-  }
-}
 
-class SearchedStateStats extends Component {
-  render() {
-    return (
-      <div className="state-stats wrapper">
-        <h2>State</h2>
         <div className="data-box">
-          <p>60900</p>
+          <p>{data.totalSamplesTested}</p>
+          <h4>Samples Tested</h4>
+        </div>
+        <div className="data-box">
+          <p>{data.totalConfirmedCases}</p>
           <h4>Confirmed Cases</h4>
         </div>
         <div className="data-box">
-          <p>7890</p>
-          <h4>Cases on Admission</h4>
+          <p>{data.totalActiveCases}</p>
+          <h4>Active Cases</h4>
         </div>
         <div className="data-box">
-          <p>74830</p>
+          <p>{data.discharged}</p>
           <h4>Discharged</h4>
         </div>
         <div className="data-box">
-          <p>89</p>
+          <p>{data.death}</p>
           <h4>Deaths</h4>
         </div>
       </div>
     );
   }
 }
+
+const SearchedStateStats = ({data}) => {
+  return (
+    <div className="state-stats wrapper">
+      <h2>{data.state}</h2>
+      <div className="data-box">
+        <p>{data.confirmedCases}</p>
+        <h4>Confirmed Cases</h4>
+      </div>
+      <div className="data-box">
+        <p>{data.casesOnAdmission}</p>
+        <h4>Cases on Admission</h4>
+      </div>
+      <div className="data-box"> 
+        <p>{data.discharged}</p>
+        <h4>Discharged</h4>
+      </div>
+      <div className="data-box">
+        <p>{data.death}</p>
+        <h4>Deaths</h4>
+      </div>
+    </div>
+  );
+};
 
 class SearchBar extends Component {
   render() {
@@ -83,15 +81,18 @@ class SearchBar extends Component {
   }
 }
 
+
+
 function Home() {
   const [covidData, setCovidData] = useState([]);
   const [userInput, setUserInput] = useState(null);
+  const [states, setStates] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault();
-    //filter through covid data with userInput,
-    //  return specific state or "Not found to SearchStateStats"
-    // fix style.
-    console.log(userInput);
+    let states = covidData.states;
+    setStates(states.filter(
+      (i) => i.state.toLowerCase() === userInput.toLowerCase()
+    ));
   };
 
   useEffect(function getData() {
@@ -99,11 +100,13 @@ function Home() {
       .then((response) => response.json())
       .then(({ data }) => {
         setCovidData(data);
+
       });
   }, []);
   //why does it log 4 times?
-  // console.log(covidData)
-  console.log(userInput)
+console.log(states)
+
+
 
   return (
     <div className="layout">
@@ -117,7 +120,8 @@ function Home() {
         {covidData === [] ? null : (
           <DisplayStats className="nationalStats" data={covidData} />
         )}
-        <SearchedStateStats className="searchStats" />
+        {/* // eslint-disable-next-line eqeqeq */}
+        { states == [] || undefined ? 'no state found' : <SearchedStateStats className="searchStats" data={states[0]} />}
       </div>
     </div>
   );
